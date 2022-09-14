f10dcf6f196313616dc348e4e60737045d6836a4
Landon Springer <landon.springer@gmail.com>
2022-09-14 18:07:52 -0500

    Initial server commit

---
+++ server/webpack.config.js
@@ -0,0 +1,64 @@
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
+
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WorkboxWebpackPlugin = require("workbox-webpack-plugin");
+// const { CheckerPlugin } = require('awesome-typescript-loader');
+
+const isProduction = process.env.NODE_ENV == "production";
+
+const config = {
+  entry: {
+    index: "./src/index.ts",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+  },
+  target: "node16.17",
+  devServer: {
+    open: true,
+    host: "localhost",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "index.html",
+    }),
+
+    // Add your plugins here
+    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/i,
+        // loader: "awesome-typescript-loader",
+        loader: "ts-loader",
+        exclude: ["/node_modules/"],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: "asset",
+      },
+
+      // Add your rules for custom modules here
+      // Learn more about loaders from https://webpack.js.org/loaders/
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".jsx", ".js"],
+  },
+};
+
+module.exports = () => {
+  if (isProduction) {
+    config.mode = "production";
+
+    config.plugins.push(new WorkboxWebpackPlugin.GenerateSW());
+  } else {
+    config.mode = "development";
+    config.watch = true;
+    config.devtool = "eval";
+    // config.plugins.push(new CheckerPlugin());
+  }
+  return config;
+};
